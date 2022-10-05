bd0ebaf8ba370136c10efa39768d5bec857c59a7
AndriiKovalchuk86 <107202100+AndriiKovalchuk86@users.noreply.github.com>
2022-10-06 00:58:30 +0300

    Update outputs.tf

--- FinalProject/terraform/WinServer/outputs.tf
+++ FinalProject/terraform/WinServer/outputs.tf
@@ -1,19 +1,19 @@
 output "resource_group_name" {
-  value = azurerm_resource_group.kir-terraform.name
+  value = azurerm_resource_group.kov-terraform.name
 }
 
 output "azurerm_windows_virtual_machine" {
-  value = azurerm_windows_virtual_machine.kir-terraform.source_image_reference
+  value = azurerm_windows_virtual_machine.kov-terraform.source_image_reference
 }
 
 output "public_ip" {
-  value = azurerm_public_ip.kir-terraform-ip.id
+  value = azurerm_public_ip.kov-terraform-ip.id
 }
 
 output "azurerm_mysql_database" {
-  value = azurerm_mysql_database.kirdb.server_name
+  value = azurerm_mysql_database.kovdb.server_name
 }
 
 output "azurerm_mysql_server" {
-  value = azurerm_mysql_server.kirmysql.name
+  value = azurerm_mysql_server.kovmysql.name
 }
