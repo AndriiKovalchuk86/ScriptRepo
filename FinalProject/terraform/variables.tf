b90c825d461999314ddc6bcefa649b9a72cc0238
AndriiKovalchuk86 <107202100+AndriiKovalchuk86@users.noreply.github.com>
2022-10-06 00:23:12 +0300

    Delete variables.tf

--- FinalProject/terraform/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "resource_group_location" {
-  default     = "westeurope"
-  description = "Location Resource Group"
-}
-
-variable "resource_group_name" {
-  default     = "azure-terraform-test"
-  description = "Resourse Group Name"
-}
